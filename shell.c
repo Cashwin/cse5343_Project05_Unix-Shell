61289666185d8b2ecd6b9321347e8f40503c2899
Cashwin <fashwinchand@smu.edu>
2013-10-21 17:04:26 -0500

    Adding a sample txt file for running shell functions

--- shell.c
+++ shell.c
@@ -1,13 +1,35 @@
-// Your Name
-// Description
-
+//Ashwin Chandrasekhar
+//Project 5- Unix Shell
 
 #include <stdio.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <stdlib.h>
 
-int main(int argc, char **argv) {
+#define BUFFSIZE 1000
 
-	//TODO
-}
+int main(int argc, char *argv[])
+{
 
-return 0;
-}
+	while(true) {
+		char *c1 = "type";
+		char *c2 = "exit";
+		char *c3 = "copy";
+		char *c4 = "delete";
+		if(strcmp(argv[1], c1) == 0) {
+		FILE *fi = fopen(argv[2], "r");             // File Pointer	
+    	char buffer[BUFFSIZE];
+    	printf("Here's your file: ");
+	    while(fgets(buffer,BUFFSIZE - 1,fi) != NULL) {
+		printf(">", argv[2]);
+        printf ("%s\n", buffer);
+	       }
+
+	    fclose(fi);
+	     }
+    break;
+	   }
+
+
+  return 0;
+ }
